79dbccc4d9f23c93631c1ba59b81f79ba4eb71b6
Josesanvil <34132595+Josesanvil@users.noreply.github.com>
2018-04-09 13:54:57 +0200

    Space line
    Added a line after mixpanel and the first function

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
     'use strict';
 
     mixpanel.track("Page charged");
+    
  
     // Smooth scrolling using jQuery easing
+    
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function() {
+        
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
